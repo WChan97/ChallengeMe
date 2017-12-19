e4d45020eceee210e99f54bc2d10560c8cefa89f
Jonathan Moallem <jdmoallem@gmail.com>
2017-12-19 12:05:24 +1100

    Clean up code, add JavaDoc comments and string-resource challenge strings

--- app/src/main/java/fyreball/challengeme/activity/HomeActivity.java
+++ app/src/main/java/fyreball/challengeme/activity/HomeActivity.java
@@ -2,7 +2,6 @@ package fyreball.challengeme.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,63 +13,40 @@ import java.util.Random;
 
 import fyreball.challengeme.R;
 
+/**
+ * An activity class that displays the random challenge and allows the user to refresh it.
+ */
 public class HomeActivity extends AppCompatActivity {
-    //
+
+    // UI fields
     private TextView mTextView;
 
-    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Define components
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //
-        mTextView = (TextView) findViewById(R.id.challengeView);
-        displayChallenge();
-        //
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mTextView = (TextView) findViewById(R.id.challengeView);
+
+        // Set up action bar
+        setSupportActionBar(toolbar);
+
+        // Display content
+        displayNewChallenge();
+
+        // Set up onclick listener
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                */
-                displayChallenge();
+                displayNewChallenge();
             }
         });
     }
-    //
-    private int getRandomNumber(int min, int max) {
-        Random rand = new Random();
-        return rand.nextInt((max - min) + 1) + min;
-    }
 
-    private String getChallenge() {
-        String challenge = "ChallengeMe";
-        int freq = getRandomNumber(1, 5);
-        if (freq == 1) {
-            challenge = "Box Jumps: " + getRandomNumber(1,5) + " Times";
-        } else if (freq == 2) {
-            challenge = "Push Ups: " + getRandomNumber(1,5) + " Times";
-        }
-        else if (freq == 3) {
-            challenge = "Sit Ups: " + getRandomNumber(1,5) + " Times";
-        }
-        else if (freq == 4) {
-            challenge = "Squats: " + getRandomNumber(1,5) + " Times";
-        }
-        else if (freq == 5) {
-            challenge = "Plank for: " + getRandomNumber(1,5) + " Minutes";
-        }
-        return challenge;
-    }
-
-    private void displayChallenge() {
-        mTextView.setText(getChallenge());
-    }
-    //
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -91,5 +67,52 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A helper method to display a new challenge onscreen.
+     */
+    private void displayNewChallenge() {
+        mTextView.setText(getChallenge());
+    }
+
+    /**
+     * A helper method to generate and return a string with a random challenge.
+     *
+     * @return the challenge string
+     */
+    private String getChallenge() {
+        String challenge = null;
+
+        int freq = getRandomNumberInRange(1, 5);
+
+        if (freq == 1) {
+            challenge = getString(R.string.challenge_box_jumps) + getRandomNumberInRange(1,5) + getString(R.string.times_suffix);
+        }
+        else if (freq == 2) {
+            challenge = getString(R.string.challenge_push_ups) + getRandomNumberInRange(1,5) + getString(R.string.times_suffix);
+        }
+        else if (freq == 3) {
+            challenge = getString(R.string.challenge_sit_ups) + getRandomNumberInRange(1,5) + getString(R.string.times_suffix);
+        }
+        else if (freq == 4) {
+            challenge = getString(R.string.challenge_squats) + getRandomNumberInRange(1,5) + getString(R.string.times_suffix);
+        }
+        else if (freq == 5) {
+            challenge = getString(R.string.challenge_planking) + getRandomNumberInRange(1,5) + getString(R.string.minutes_suffix);
+        }
+        return challenge;
+    }
+
+    /**
+     * A helper method to generate and return a random number between two values.
+     *
+     * @param min - the minimum value
+     * @param max - the maximum value
+     * @return the random value
+     */
+    private int getRandomNumberInRange(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
